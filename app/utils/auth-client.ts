8f53eda30922f8ca0afa3ccad553cf0ba893f9d2
orderlyproblemsolvers <orderlyproblemsolvers@gmail.com>
2025-12-29 16:13:05 +0100

    layout added in login form & trusted origin set as true domain

--- app/utils/auth-client.ts
+++ app/utils/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/vue";
 
 export const authClient = createAuthClient({
-    baseURL: "https://orderlyproblemsolvers.com " // Ensure this matches your .env BETTER_AUTH_URL
+    baseURL: "https://orderlyproblemsolvers.com" // Ensure this matches your .env BETTER_AUTH_URL
 });
 
 // We rename 'useSession' to 'useAuthSession' to avoid collision with Nuxt's built-in useSession
